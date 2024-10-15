3fd70c3794f182335d2bf022b7e0d6d2c35ad30b
Nev <ngushteraklieva@gmail.com>
2024-10-15 17:12:39 +0200

    Implement DecisionCard component for displaying decisions

---
+++ enterprise-decision-tracker/src/components/DecisionCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <a href="#" class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{title}}</h5>
+        <p class="font-normal text-gray-700 mb-2">{{ description }}</p>
+        <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer">
+            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+            <span class="ms-3 text-sm font-medium text-gray-900">{{ status }}</span>
+        </label>
+    </a>
+</template>
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    status: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
