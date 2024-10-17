0c313c229835ad06b7829e609cc432e94e193b5b
Nev <ngushteraklieva@gmail.com>
2024-10-17 13:19:44 +0200

    Add reusable Toggle Button Component

---
+++ enterprise-decision-tracker/src/components/ToggleButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <div @click.stop class="flex">
+                <label class="inline-flex items-center w-1/3 cursor-pointer">
+                <input type="checkbox" class="sr-only peer" @change="handleToggle" :checked="props.checked">
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                <span class="ms-3 text-sm font-medium text-gray-900">{{ currentStatus }}</span>
+                </label>
+                <slot></slot>
+            </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    checked: {
+        type: Boolean,
+        default: false
+    },
+    currentStatus: String
+})
+
+const emit = defineEmits(['update:status']);
+
+// Handle the toggle event and emit the new state
+const handleToggle = (event) => {
+    emit('update:status', event.target.checked); // Emit the new value to the parent
+};
+
+</script>
