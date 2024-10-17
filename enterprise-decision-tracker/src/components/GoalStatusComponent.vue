89631a6a5956203426cbf9a12b1b542e5d5778f5
Nev <ngushteraklieva@gmail.com>
2024-10-17 13:20:33 +0200

    Add a component for managing the goal status

---
+++ enterprise-decision-tracker/src/components/GoalStatusComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="isMet !== null" class="flex mb-3">
+        <font-awesome-icon :class="statusClass" class="text-lg m-auto" :icon="statusIcon" />
+        <p class="w-full pl-2">{{ statusMessage }}</p>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    isMet: {
+        type: Boolean,
+        required: true
+    },
+    measurableGoal: Number
+});
+
+const statusClass = computed(() => (props.isMet === true ? 'text-emerald-500' : 'text-red-500'));
+const statusIcon = computed(() => (props.isMet === true ? ['fas', 'circle-check'] : ['fas', 'circle-xmark']));
+const statusMessage = computed(() => (props.isMet === true ? `The goal was met! The revenue was increased by ${props.measurableGoal}%.` : `The goal of increasing the revenue by ${props.measurableGoal}% was not met.`));
+</script>
