10c7611940d416d3996bae804bf94eb1283adc79
Nev <ngushteraklieva@gmail.com>
2024-10-17 13:20:58 +0200

    Add reusable Alert Modal

---
+++ enterprise-decision-tracker/src/components/AlertModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <!-- Modal content -->
+      <div class="relative w-full max-w-lg bg-white rounded-lg shadow">
+        <button @click="closeModal" class="absolute top-3 right-3 text-gray-400 hover:text-gray-900">
+          &times;
+        </button>
+        <div class="p-6">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    showModal:{
+        type: Boolean,
+        default:false
+    }
+})
+const emit = defineEmits(['close-modal']);
+ 
+// Close the modal when clicked outside
+const closeModal = () => {
+   emit('close-modal');
+};
+</script>
+  
