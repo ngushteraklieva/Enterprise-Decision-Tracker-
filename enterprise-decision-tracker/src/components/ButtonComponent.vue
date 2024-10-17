57875b334544a9cf746fcbc56ffef88d304c0b0e
Nev <ngushteraklieva@gmail.com>
2024-10-17 13:19:12 +0200

    Add reusable button component

---
+++ enterprise-decision-tracker/src/components/ButtonComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex w-full p-5 justify-end items-center">
+        <button @click="handleButtonAction"><font-awesome-icon class="text-lg p-2" :icon="['fas', icon]" /></button>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    icon: String
+})
+
+const emit = defineEmits(['button-action'])
+
+const handleButtonAction = () => {
+    emit('button-action')
+}
+</script>
